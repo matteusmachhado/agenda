a94cd443f58ed663ea101cf3b1a4f057cd8d85f9
Mateus Machado <matteusmachhado@hotmail.com>
2024-04-13 09:48:21 -0300

    fead: implementation to create user client

--- src/Agenda.Domain/Features/Client/Commands/Create/CreateCommandHandler.cs
+++ src/Agenda.Domain/Features/Client/Commands/Create/CreateCommandHandler.cs
@@ -1,20 +1,46 @@
 ﻿using Agenda.Data.Interfaces;
+using Agenda.Shared.Enums;
 using FluentValidation.Results;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace Agenda.Domain.Features.Client.Commands.Create
 {
     public class CreateCommandHandler : BaseCommandHandler, IRequestHandler<CreateCommand, ValidationResult>
     {
-        public CreateCommandHandler(IUnitOfWork uow) : base(uow)
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public CreateCommandHandler(IUnitOfWork uow,
+            UserManager<IdentityUser> userManager) : base(uow)
         {
+            _userManager = userManager;
         }
 
         public async Task<ValidationResult> Handle(CreateCommand request, CancellationToken cancellationToken)
         {
             if (!request.IsValid()) return request.ValidationResult;
 
+            var user = new IdentityUser
+            {
+                UserName = Guid.NewGuid().ToString()
+            };
 
+            switch (request.TypeOfCheck)
+            {
+                case TypeOfCheckEnum.SMS:
+                    user.PhoneNumber = request.From;
+                    user.PhoneNumberConfirmed = true;
+                    break;
+                case TypeOfCheckEnum.Email: 
+                    user.Email = request.From;
+                    user.EmailConfirmed = true;
+                    break;
+            }
+
+            // adicionar validação por phonenumber
+
+            await _userManager.CreateAsync(user);
 
             return request.ValidationResult;
         }
--- src/Agenda.Domain/Notifications/Client/ClientEventHandler.cs
+++ src/Agenda.Domain/Notifications/Client/ClientEventHandler.cs
@@ -1,4 +1,8 @@
-﻿using MediatR;
+﻿using Agenda.Entities;
+using Agenda.Shared.Enums;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +13,40 @@ namespace Agenda.Domain.Notifications.Client
 {
     public class ClientEventHandler : INotificationHandler<ClientVerificationCodeEvent>
     {
-        public Task Handle(ClientVerificationCodeEvent notification, CancellationToken cancellationToken)
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public ClientEventHandler(UserManager<IdentityUser> userManager, 
+            SignInManager<IdentityUser> signInManager)
         {
-            return Task.CompletedTask;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task Handle(ClientVerificationCodeEvent notification, CancellationToken cancellationToken)
+        {
+            var client = await FindClientUserAsync(notification.TypeOfCheck, notification.From);
+            if (client is not null)
+            {
+                await _signInManager.SignInAsync(client, false);
+            }
+        }
+
+        private async Task<IdentityUser> FindClientUserAsync(TypeOfCheckEnum typeOfCheck, string from)
+        {
+            IdentityUser user = null;
+
+            switch (typeOfCheck)
+            {
+                case TypeOfCheckEnum.SMS:
+                    user = await _userManager.Users.FirstOrDefaultAsync(u => u.PhoneNumber != null && u.PhoneNumber.Equals(from));
+                    break;
+                case TypeOfCheckEnum.Email:
+                    user = await _userManager.FindByEmailAsync(from);
+                    break;
+            }
+
+            return user;
         }
     }
 }
